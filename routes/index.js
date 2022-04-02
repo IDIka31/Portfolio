1db1a2e797f4113af4046eb70bb9421f8f614cfc
Andika <andikanugraha17@smp.belajar.id>
2022-04-02 22:11:57 +0800

    Memberbaiki error auth

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,10 @@ app.post('/', async (req, res) => {
     const body = req.body;
 
     const transporter = nodemailer.createTransport({
-        host: 'smtp.mailgun.org',
-        port: 587,
-        secure: false,
+        service: 'gmail',
         auth: {
-            user: 'postmaster@sandbox78701b9f532340a59c3048c718003477.mailgun.org',
-            pass: '9655f63086425ae697698ed818b22733-62916a6c-1e1d8f4c',
+            user: 'idikanugraha@gmail.com',
+            pass: 'vmbjdjxncjbfulpw',
         },
     });
 
